ddec4e10a6b572e6c572a0cd2f67c8aeb82f15de
sayfulla <ksayfula@mail.ru>
2019-12-14 01:30:05 +0300

    master Написание тестового запроса к методу ping веб-сервиса Metroservice

--- postReguests.py
+++ postReguests.py
@@ -1,36 +1,30 @@
 import requests
+import xml.etree.ElementTree as etree
+import json
 
 xml = bytearray(b'''<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
     <Ping xmlns="http://MetroService/">
-      <login>string</login>
-      <password>string</password>
+      <login>1234admin1234metro1234service</login>
+      <password>43211234</password>
     </Ping>
   </soap:Body>
 </soap:Envelope>''')
 
 headers = {'Content-Type': 'text/xml', 'SOAPAction': "http://MetroService/Ping"}
 resp = requests.post("http://metroservice.somee.com/WebService/MetroService.asmx?op=Ping", data=xml, headers=headers).text
-print(resp)
+with open('www.xml', 'w', encoding='utf-8') as f:
+    f.write(resp)
 
+context = etree.iterparse('www.xml', events=('end',))
 
+for event, elem in context:
+    if type(elem.text) == str:
+        jObjStr = elem.text
 
+jDict = json.loads(jObjStr)
+print(jDict['error'], ':', jDict['message'])
 
-
-#print(resp)
-#
-## инициируем запрос на получение токена
-#r = requests.post("https://api.artsy.net/api/tokens/xapp_token",
-#                  data={
-#                      "client_id": client_id,
-#                      "client_secret": client_secret
-#                  })
-#
-## разбираем ответ сервера
-#j = json.loads(r.text)
-#
-## достаем токен
-#token = j["token"]
 
 
